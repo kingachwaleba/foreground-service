da689b35be0e70a6ab156dcd4452d493438a24ef
kingachwaleba <kingach6@gmail.com>
2021-11-23 11:24:39 +0100

    Create main layout

--- app/src/main/java/com/example/foregroundservice/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = findViewById(R.id.buttonStart);
+        Button buttonStop = findViewById(R.id.buttonStop);
+        Button buttonRestart = findViewById(R.id.buttonRestart);
+
+        buttonStart.setOnClickListener(this::clickStart);
+        buttonStop.setOnClickListener(this::clickStop);
+        buttonRestart.setOnClickListener(this::clickRestart);
     }
 
     @Override
@@ -34,5 +45,18 @@ public class MainActivity extends AppCompatActivity {
         }
         else
             return super.onOptionsItemSelected(item);
+    }
+
+    public void clickStart(View view) {
+        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickStop(View view) {
+        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickRestart(View view) {
+        clickStop(view);
+        clickStart(view);
     }
 }
