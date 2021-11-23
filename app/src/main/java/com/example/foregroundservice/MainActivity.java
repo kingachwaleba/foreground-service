b39d7fa5dd561bd74392b190d42270445e425d92
kingachwaleba <kingach6@gmail.com>
2021-11-23 11:09:45 +0100

    Create menu

--- app/src/main/java/com/example/foregroundservice/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.foregroundservice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemSettings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        else if (item.getItemId() == R.id.itemExit) {
+            finishAndRemoveTask();
+            return true;
+        }
+        else
+            return super.onOptionsItemSelected(item);
     }
 }
